623aa09dd14fae7c25e697853c3f7064991181d1
Hender Escorcia <hender_escorcia@hotmail.com>
2026-03-01 18:43:07 -0500

    Registramos canciones

---
+++ main.py
@@ -0,0 +1,14 @@
+print("==" * 13)
+print("Canciones Favoritas")
+print("==" * 13)
+
+cancion1 = (input("Ingresa tu primera cancion favorita: "))
+cancion2 = (input("Ingresa tu segunda cancion favorita: "))
+cancion3 = (input("Ingresa tu tercera cancion favorita: "))
+
+print("==" * 15)
+print("Tus canciones favoritas son: ")
+print("1: ", cancion1)
+print("2: ", cancion2)
+print("3: ", cancion3)
+print("==" * 15)
